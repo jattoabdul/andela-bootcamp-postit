da52ea260f89c63896d5cf13404057e7fd7388cd
jattoabdul <jattoade@gmail.com>
2017-08-10 00:33:18 +0100

    added jest-enzyme to simplify testing

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "./../components/App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(< App />, div);
-});
 
 describe("App", () => {
   it("should properly run tests", () => {
     expect(1).toBe(1);
   });
 });
+
